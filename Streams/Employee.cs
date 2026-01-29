ebbb9ce9c15ce611535dbee72e6848e2ccd572ea
abhay9876 <singhabhayrna987@gmail.com>
2026-01-29 12:27:21 +0530

    [Abhay Singh] Add . Employee Class

---
+++ Streams/Employee.cs
@@ -0,0 +1,10 @@
+namespace Streams
+{
+    public class Employee
+    {
+        public int Id;
+        public string Name;
+        public string Department;
+        public double Salary;
+    }
+}
