fca6737cc1006701581f6761a6e36be613a63e66
abhay9876 <singhabhayrna987@gmail.com>
2026-01-29 12:56:08 +0530

    [Abhay Singh] Add. Candidate Class

---
+++ CSV/ValidateData/Candidate.cs
@@ -0,0 +1,10 @@
+namespace ValidateData
+{
+    public class Candidate
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+
+}
