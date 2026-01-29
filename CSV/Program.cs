5ac0b209f3ede1db45ac1d038585baa198870cdf
abhay9876 <singhabhayrna987@gmail.com>
2026-01-29 12:58:28 +0530

    [Abhay Singh] Add. csv handling problems

---
+++ CSV/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ModifyData;
+using SortRecords;
+using ValidateData;
+using DataToObject;
+using MergeFiles;
+using DetectDuplicate;
+
+namespace CSV
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            //ReadCsv.Perform();
+
+            //WriteData.Perform();
+
+            //CountRows.Perform();
+
+            //FilterRecords.FilterPerform();
+
+            //SerachRecord.Search();
+
+            //ModifingData.Perform();
+
+            //SortData.Perform();
+
+            //ValidateProcess.Validate();
+
+            //ConvertData.Convert();
+
+            //MergedFiles.Merge();
+
+            //FindDuplicate.Find();
+
+
+
+
+        }
+    }
+}
