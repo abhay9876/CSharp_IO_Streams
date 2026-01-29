b08bda2c0347fd583f65862450cff99c3ff78aa6
abhay9876 <singhabhayrna987@gmail.com>
2026-01-29 12:32:27 +0530

    [Abhay Singh] Add . Stream handling Problems Solved

---
+++ Streams/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using Streams;
+namespace FileHandling
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // ReadWrite rw = new ReadWrite();
+            // rw.Operations();
+
+            // ReadInput r = new ReadInput();
+            // r.Perform();
+
+            // SaveRetrive sr = new SaveRetrive();
+            // sr.Operations();
+
+            // ConvertImage ci = new ConvertImage();
+            // ci.Operation();
+
+            // ConvertCases cc = new ConvertCases();
+            // cc.Perform();
+
+            // PrimitiveData pd = new PrimitiveData();
+            // pd.Perform();
+
+            // ThreadCommunication tc = new ThreadCommunication();
+            // tc.Operations();
+
+            // LargeFile lf = new LargeFile();
+            // lf.ReadPerform();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
