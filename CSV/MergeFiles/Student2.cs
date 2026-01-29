9b7d0562365953a02facb07817452fac158c837c
abhay9876 <singhabhayrna987@gmail.com>
2026-01-29 12:48:41 +0530

    [Abhay Singh] Add. Students class second file

---
+++ CSV/MergeFiles/Student2.cs
@@ -0,0 +1,9 @@
+namespace MergeFiles
+{
+    public class Student2
+    {
+        public int Id { get; set; }
+        public int Marks { get; set; }
+        public string Grade { get; set; }
+    }
+}
