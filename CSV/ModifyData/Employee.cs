299b67acec5acb7595f8b955292c02cb8876776c
abhay9876 <singhabhayrna987@gmail.com>
2026-01-29 12:50:09 +0530

    [Abhay Singh] Add. Employee Class

---
+++ CSV/ModifyData/Employee.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ModifyData
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public double Salary { get; set; }
+    }
+}
