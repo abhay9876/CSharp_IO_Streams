b43b75bba3a5e4595c73d3ffec1e993cd7d9a1b5
abhay9876 <singhabhayrna987@gmail.com>
2026-01-29 12:47:07 +0530

    [Abhay Singh] Add. First Student Structure

---
+++ CSV/MergeFiles/Student1.cs
@@ -0,0 +1,9 @@
+namespace MergeFiles
+{
+    public class Student1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
